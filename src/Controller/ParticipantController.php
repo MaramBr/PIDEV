deebc93989a6bc7afdc95de52b01e0466cb7d34f
Emna <emna.abbessi@esprit.tn>
2023-03-04 01:27:43 +0100

    second c

---
+++ src/Controller/ParticipantController.php
@@ -0,0 +1,327 @@
+<?php
+
+namespace App\Controller;
+use Symfony\Component\Mailer\Transport\Smtp\SmtpTransport;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Participant;
+use App\Entity\Evenement;//controller
+use Doctrine\Persistence\ManagerRegistry;//controller
+use App\Repository\ParticipantRepository;//controller
+use Symfony\Component\Mailer\Transport;
+use App\Repository\EvenementRepository;
+use Symfony\Component\Mailer\Transport\Smtp\Stream\SocketStream;
+use App\Form\ParticipantType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+//use Symfony\Component\Validator\Constraints as Assert;
+// use Symfony\Component\Validator\Constraints\Email;
+
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\HttpFoundation\Session\Session;
+use App\Form\ForgotPasswordType;
+use App\Repository\UserRepository;
+use App\Form\UserType;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use Swift_Mailer;
+use Swift_Message;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Csrf\TokenGenerator\TokenGeneratorInterface;
+use Dompdf\Dompdf;
+use Dompdf\Options;
+use PHPMailer\PHPMailer\PHPMailer;
+use Twilio\Rest\Client;
+
+
+ 
+class ParticipantController extends AbstractController
+{
+    #[Route('/Participant', name: 'app_Participant')]
+    public function index(): Response
+    {
+        return $this->render('Participant/index.html.twig', [
+            'controller_name' => 'ParticipantController',
+        ]);
+    }
+
+   
+    
+    #[Route('/Participant/afficher', name: 'appback3')]
+    public function afficheback(ManagerRegistry $em): Response
+    {
+        $repo=$em->getRepository(Participant::class);
+        $result=$repo->findAll();
+        return $this->render ('Participant/annuler.html.twig',['Participant'=>$result]);
+   
+       
+    }
+ 
+    #[Route('/Participant/afficherback', name: 'affichefront1')]
+    public function affiche6(ManagerRegistry $em): Response
+    {
+        $repo=$em->getRepository(Participant::class);
+        $result=$repo->findAll();
+        return $this->render ('Participant/back3.html.twig',['Participant'=>$result]);
+   
+       
+    }
+
+
+
+   #[Route('/Participant/add', name: 'add3')]
+public function add(MailerInterface $mailer, ManagerRegistry $doctrine, Request $request): Response
+{
+    $participant = new Participant();
+    $form = $this->createForm(ParticipantType::class, $participant);
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+        $em = $doctrine->getManager();
+
+        // Get the events associated with the participation
+        $events = $form->get('evenement')->getData();
+
+        foreach ($events as $evenement) {
+
+            // Check if the number of participants for the event is greater than zero
+            if ($evenement->getNbParticipant() <= 0) {
+                $this->addFlash(
+                    'error',
+                    'Impossible d\'ajouter un participant à l\'événement ' . $evenement->getNom() . ' car le nombre maximum de participants est atteint.'
+                );
+                return $this->redirectToRoute('add3');
+            }
+            // Update the number of participants for the event
+            $evenement->setNbParticipant($evenement->getNbParticipant() - 1);
+
+            // Associate the participant with the event
+            $participant->addEvenement($evenement);
+
+            // Save the event to the database
+            $em->persist($evenement);
+        }
+
+        // Save the participant to the database
+        $em->persist($participant);
+        $em->flush();
+
+        $this->addFlash(
+            'addP',
+            'Participation sauvegardée avec succès.'
+        );
+
+        //////////////////////SMS////////////////////////////////
+          
+ $accountSid = 'AC92f7e404547cf2736427dd218cd01a28';
+            $authToken = 'aa5eff3ac65bb9325e475404c37f303f';
+            $client = new Client($accountSid, $authToken);
+    
+            $message = $client->messages->create(
+                '+21627085182', // replace with admin's phone number
+                [
+                    'from' => '+12764009477', // replace with your Twilio phone number
+                    'body' => 'un nouveau participant ' . $form->get('nom')->getData(),
+                ]
+            );
+
+
+  /////////////////////////Notifier Participant/////////////////////////////     
+    $mailer = new PHPMailer();
+    $mailer->isSMTP();
+    $mailer->SMTPSecure = 'tls';
+    $mailer->SMTPAutoTLS = false;
+    $mailer->Host = 'smtp.gmail.com';
+    $mailer->Port = 587;
+    $mailer->SMTPAuth = true;
+    $mailer->Username = 'emna.abbessi@esprit.tn';
+    $mailer->Password = '12715163';
+    $mailer->setFrom('E-Fit');
+    $mailer->addAddress($participant->getEmail());
+   
+    
+    $mailer->Subject = 'Participation';
+    $mailer->Body = 'Participation chez ' .$evenement->getNom().' avec succes';
+    
+    if (!$mailer->send()) {
+        // Gestion des erreurs d'envoi de l'e-mail
+        $this->addFlash('danger', 'Une erreur est survenue lors de l\'envoi de l\'e-mail de notification.');
+    } else {
+        // Succès de l'envoi de l'e-mail
+        $this->addFlash('success', 'L\'événement a été supprimé avec succès et un e-mail de notification a été envoyé.');
+    }
+
+
+
+
+        return $this->redirectToRoute('afficher', ['id' => $participant->getId()]);
+    }
+
+    return $this->render('Participant/add3.html.twig', [
+        'formParticipant' => $form->createView(),
+    ]);
+}
+
+     
+   
+
+    #[Route('/Participant/update/{id}', name: 'update3')]
+
+    public function  updateParticipant (ManagerRegistry $doctrine,$id,  Request  $request) : Response
+    { $Participant = $doctrine
+
+        ->getRepository(Participant::class)
+        ->find($id);
+        $form = $this->createForm(ParticipantType::class, $Participant);
+        $form->add('update', SubmitType::class) ;
+        $form->handleRequest($request);
+        if ($form->isSubmitted())
+        { $em = $doctrine->getManager();
+            $em->flush();
+           
+       return $this->redirectToRoute('afficher', ['id' => $Participant->getId()]);
+        }
+        return $this->renderForm("Participant/add3.html.twig",
+            ["formParticipant"=>$form]) ;
+
+
+    } 
+
+    #[Route('/Participant/delete/{id}', name: 'delete3')]
+
+    public function delete($id, ManagerRegistry $doctrine)
+{
+    $participant = $doctrine
+        ->getRepository(Participant::class)
+        ->find($id);
+
+    $em = $doctrine->getManager();
+    $em->remove($participant);
+
+    // Get the events associated with the participation
+    $events = $participant->getEvenement();
+
+    foreach ($events as $evenement) {
+        // Increment the nbParticipant attribute of the event
+        $evenement->setNbParticipant($evenement->getNbParticipant() + 1);
+
+        // Save the event to the database
+        $em->persist($evenement);
+    }
+
+    $em->flush();
+
+    $this->addFlash(
+        'delP',
+        'Participation supprimée avec succès'
+    );
+
+    return $this->redirectToRoute('appback3');
+}
+
+
+
+
+     #[Route('/Participant/delete2/{id}', name: 'delete5')]
+
+    public function delete2($id, ManagerRegistry $doctrine)
+{
+    $participant = $doctrine
+        ->getRepository(Participant::class)
+        ->find($id);
+
+    $em = $doctrine->getManager();
+    $em->remove($participant);
+
+    // Get the events associated with the participation
+    $events = $participant->getEvenement();
+
+    foreach ($events as $evenement) {
+        // Increment the nbParticipant attribute of the event
+        $evenement->setNbParticipant($evenement->getNbParticipant() + 1);
+
+        // Save the event to the database
+        $em->persist($evenement);
+    }
+
+    $em->flush();
+
+    $this->addFlash(
+        'delP',
+        'Participation supprimée avec succès'
+    );
+
+    return $this->redirectToRoute('affichefront1');
+}
+
+
+ 
+  #[Route('/Participant/affiche/{id}', name: 'afficher')]
+ public function showAction(int $id): Response
+{
+    $participant = $this->getDoctrine()->getRepository(Participant::class)->find($id);
+
+    if (!$participant) {
+        throw $this->createNotFoundException('Participant introuvable');
+    }
+
+    return $this->render('participant/affich.html.twig', [
+        'participant' => $participant,
+    ]);
+}
+
+ 
+
+
+    /**
+ * @Route("/participant/pdfP/{id}", name="pdfP")
+ */
+public function pdfP(ParticipantRepository $repository, $id): Response
+{
+    // Configure Dompdf according to your needs
+    $pdfOptions = new Options();
+    $pdfOptions->set('defaultFont', 'Arial');
+
+    // Instantiate Dompdf with our options
+    $dompdf = new Dompdf($pdfOptions);
+    $participant = $repository->find($id);
+
+    // Retrieve the HTML generated in our twig file
+    $html = $this->renderView('Participant/pdf.html.twig', [
+        'participant' => $participant = $this->getDoctrine()->getRepository(Participant::class)->find($id)
+    ]);
+
+    // Load HTML to Dompdf
+    $dompdf->loadHtml($html);
+
+    // (Optional) Setup the paper size and orientation 'portrait' or 'portrait'
+    $dompdf->setPaper('A4', 'portrait');
+
+    // Render the HTML as PDF
+    $dompdf->render();
+
+    // Output the generated PDF to Browser (force download)
+    $dompdf->stream("mypdf.pdf", [
+        "Attachment" => false
+    ]);
+    exit(0);
+}
+  
+
+ #[Route('/searchParticipant', name: 'searchParticipant')]
+public function searchParticipantx(Request $request, NormalizerInterface $Normalizer, ParticipantRepository $sr)
+{
+    $repository = $this->getDoctrine()->getRepository(Participant::class);
+    $requestString = $request->get('searchValue');
+    $Participants = $repository->findParticipantByNom($requestString);
+    $jsonContent = $Normalizer->normalize($Participants, 'json', ['groups' => 'Participant']);
+    $retour = json_encode($jsonContent);
+    return new Response($retour);
+}
+
+}
---
+++ src/Entity/Participant.php
@@ -0,0 +1,185 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ParticipantRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ParticipantRepository")
+ * @UniqueEntity(fields={"email"}, message="Cet email est déjà utilisé par un autre participant.")
+ * @UniqueEntity(fields={"tel"}, message="Ce numero est déjà utilisé par un autre participant.")
+ */
+
+#[ORM\Entity(repositoryClass: ParticipantRepository::class)]
+class Participant
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message:"nom evenement doit etre non vide")]
+    #[Assert\Regex(
+         pattern:"/^[^0-9]+$/",
+         message:"Le nom ne doit pas contenir de chiffres"
+     )]
+
+     #[Groups("Participant")]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message:"nom evenement doit etre non vide")]
+    #[Assert\Regex(
+         pattern:"/^[^0-9]+$/",
+         message:"Le nom ne doit pas contenir de chiffres"
+     )]
+     #[Groups("Participant")]
+    private ?string $prenom = null;
+
+    #[ORM\Column(length: 255)]
+     #[Assert\NotBlank(message:"l'email doit etre non vide")]
+     #[Assert\Email(message:"L'email n'est pas valide ")]
+     #[Groups("Participant")]
+    private ?string $email = null;
+
+    #[ORM\Column]
+ #[Assert\NotBlank(message:"AGE doit etre non vide")]
+ 
+#[Groups("Participant")]
+    private ?int $age = null;
+
+ 
+ #[ORM\Column]
+    #[Groups("Participant")]
+    #[Assert\NotBlank(message : "Le numéro de téléphone est obligatoire.")]
+    #[Assert\Length(
+        max : 11,
+        maxMessage : "Le numéro de téléphone ne doit pas contenir plus de {{ limit }} chiffres."
+    )]
+    private ?string $tel = null;
+
+
+    #[ORM\ManyToMany(targetEntity: Evenement::class, inversedBy: 'participants')]
+
+    private Collection $evenement;
+
+    
+    public function __construct()
+    {
+        $this->evenements = new ArrayCollection();
+        $this->evenementParticipants = new ArrayCollection();
+        $this->evenement = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): self
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getAge(): ?int
+    {
+        return $this->age;
+    }
+
+    public function setAge(int $age): self
+    {
+        $this->age = $age;
+
+        return $this;
+    }
+
+    
+  public function __toString()
+    {
+        return (string) $this->getNom();
+        
+        $this->getPrenom();
+        $this->getEmail();
+        $this->getAge();
+        $this->getTel();
+      
+        
+    }
+  
+    public function getTel(): ?int
+    {
+        return $this->tel;
+    }
+
+    public function setTel(int $tel): self
+    {
+        $this->tel = $tel;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Evenement>
+     */
+    public function getEvenement(): Collection
+    {
+        return $this->evenement;
+    }
+
+    public function addEvenement(Evenement $evenement): self
+    {
+        if (!$this->evenement->contains($evenement)) {
+            $this->evenement->add($evenement);
+        }
+
+        return $this;
+    }
+
+    public function removeEvenement(Evenement $evenement): self
+    {
+        $this->evenement->removeElement($evenement);
+
+        return $this;
+    }
+
+    
+}
